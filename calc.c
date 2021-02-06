7a1640e62413fd747b3c27a0cb5e7c10e983b314
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-02-06 20:19:50 -0300

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,64 @@
+      .--..--..--..--..--..--.
+    .' \  (`._   (_)     _   \
+  .'    |  '._)         (_)  |
+  \ _.')\      .----..---.   /
+  |(_.'  |    /    .-\-.  \  |
+  \     0|    |   ( O| O) | o|
+   |  _  |  .--.____.'._.-.  |
+   \ (_) | o         -` .-`  |
+    |    \   |`-._ _ _ _ _\ /
+    \    |   |  `. |_||_|   |     Solution!
+    | o  |    \_      \     |     -.   .-.
+    |.-.  \     `--..-'   O |     `.`-' .'
+  _.'  .' |     `-.-'      /-.__   ' .-'
+.' `-.` '.|='=.='=.='=.='=|._/_ `-'.'
+`-._  `.  |________/\_____|    `-.'
+   .'   ).| '=' '='\/ '=' |
+   `._.`  '---------------'
+           //___\   //___\
+             ||       ||
+             ||_.-.   ||_.-.
+            (_.--__) (_.--__)
+*/
+// biblioteca de padrão de entrada e saida, standart input and output
+#include <stdio.h>
+
+//declaração das variaveis
+char  line[100],operator; 
+int   result,value;
+
+// Banner 
+char opcao_menu() {
+ int i;
+ char *banner[] = {
+  "Operator Calc",
+  "coded by Cooler_  visit http://botecounix.com.br",
+  "put with example",
+  "operators:+,-,*,/ ",
+  "(s)para Sair",
+  "-> ",
+ };
+// \x0a é a mesma coisa que \n para pular linha só que mais rapido como é hex
+ for(i=0; i<=5; i++) printf(" %s \x0a",banner[i]);
+}
+   
+int main() {
+// chama o banner  e seta resultado para zero
+    opcao_menu(); result = 0; 
+    while (1) {
+     printf("Resultado: %d\x0a", result );
+     printf("Digite Operador e o numero : ");
+// pega as duas entradas
+     fgets(line, sizeof(line), stdin);
+     sscanf(line, "%c %d", &operator, &value);
+// se a entrada for s ou S programa sai
+     if ((operator == 's') || (operator == 'S')) break;
+//condições para verificar operador e fazer a conta com ultimo numero armazenado
+// fazer result += value é a mesma coisa que fazer "result=result+value" soh que é mais rapido
+     if (operator == '+') result += value;
+     if (operator == '/') result /= value;
+     if (operator == '-') result -= value;
+     if (operator == '*') result *= value;
+   } 
+ return(0);
+}
